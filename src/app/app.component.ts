853b01522e1708f80e30a30e48c348c9bfa1bb8a
jeroperales <jeronimoperales1@gmail.com>
2024-10-28 22:22:14 -0300

    actualize css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InterfazComponent } from './components/interfaz/interfaz.component';
+import { GridComponent } from './components/grid/grid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InterfazComponent],
+  imports: [
+    RouterOutlet, 
+    InterfazComponent,
+    GridComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
