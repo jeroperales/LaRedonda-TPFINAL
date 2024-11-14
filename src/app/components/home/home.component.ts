46fc81c50ecc190a02498699e58552b586a1d8f3
GianSicolo <gianxd2002@gmail.com>
2024-11-14 19:11:58 -0300

    Comit  de barra busqueda, login,  register y forum

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,40 @@
-
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { EquiposService } from '../../services/equipos.service';
+import { Router } from '@angular/router';
+import { Equipo } from '../../interfaces/equipo-interface';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent {
+  constructor(private equipoService: EquiposService, private router: Router) {}
+
+nombreEquipo: string = '';
+
+buscarEquipo(): void {
+  if (this.nombreEquipo.trim() !== '') {
+    this.equipoService.getEquipoByNombre(this.nombreEquipo).subscribe(
+      equipo => {
+        if (equipo) {
+          this.router.navigate(['/detalles', equipo.id, { league: equipo.league }]);
+        } else {
+          console.log('Equipo no encontrado');
+          alert("Equipo no encontrado");
+        }
+      },
+      error => {
+        console.error('Error al buscar el equipo:', error);
+      }
+    );
+  }
+}
+
 
 }
